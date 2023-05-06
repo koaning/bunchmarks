642c0020f3bca426b15b19a97e275c60b62e1f6c
vincent d warmerdam <vincentwarmerdam@gmail.com>
2023-05-06 15:54:29 +0000

    v0

--- bunchmarks/__init__.py
+++ bunchmarks/__init__.py
@@ -6,18 +6,26 @@ text_clf_mapper = {
         "label_col": "label",
         "test_set": "train",
         "test_set": "test"
+    }, 
+    "imdb": {
+        "feature_col": "text",
+        "label_col": "label",
+        "test_set": "train",
+        "test_set": "test"
+    },
+    "rotten_tomatoes": {
+        "feature_col": "text",
+        "label_col": "label",
+        "test_set": "train",
+        "test_set": "test"
     }
 }
 
-def fetch_dataset(name, n_train=None):
+def fetch_dataset(name):
     p = text_clf_mapper[name]
     dataset = load_dataset(name)
-    X_train = dataset[p['train_set']][p['feature_col']][:n_train]
-    y_train = dataset[p['train_set']][p['label_col']][:n_train]
+    X_train = dataset[p['train_set']][p['feature_col']]
+    y_train = dataset[p['train_set']][p['label_col']]
     X_test = dataset[p['test_set']][p['feature_col']]
     y_test = dataset[p['test_set']][p['label_col']]
     return X_train, y_train, X_test, y_test
-
-
-def fetch_agnews(n_train):
-    return fetch_dataset("ag_news")
